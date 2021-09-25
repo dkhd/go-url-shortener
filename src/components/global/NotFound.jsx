cbc3da02264cfcb527398290f7fa000b5a4b443d
Diky Hadna <dikyhd@gmail.com>
2021-09-25 17:37:01 +0700

    Adjusting interstitial page for mobile

--- src/components/global/NotFound.jsx
+++ src/components/global/NotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col w-full h-screen bg-dkhd-purple justify-center items-center">
+    <div className="flex flex-col w-full h-screen bg-dkhd-purple justify-center items-center px-10">
       <span className="md:w-4/12 text-3xl text-dkhd-purple-200 font-light mt-10 text-center leading-relaxed text-center">
         404 - Short URL not found!
       </span>
--- src/components/global/PleaseWait.jsx
+++ src/components/global/PleaseWait.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PleaseWait = () => {
   return (
-    <div className="flex flex-col w-full h-screen bg-dkhd-purple justify-center items-center">
+    <div className="flex flex-col w-full h-screen bg-dkhd-purple justify-center items-center px-10">
       <span className="md:w-4/12 text-3xl text-dkhd-purple-200 font-light mt-10 text-center leading-relaxed text-center">
         Please wait while you are being redirected...
       </span>
